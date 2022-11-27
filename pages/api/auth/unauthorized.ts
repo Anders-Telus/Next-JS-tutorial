b39f9108cd90ba8c6c8b685ce8443389cf6da3dd
Andres Lind <andreslind@MLIVE-MAC2001.local>
2022-11-27 16:47:53 -0500

    AG-269 clean up

--- pages/api/auth/unauthorized.ts
+++ pages/api/auth/unauthorized.ts
@@ -1,12 +1,7 @@
 import type { NextApiRequest, NextApiResponse } from 'next';
-import { User, users } from "../../../data/mockUsers";
 
-type Data = {
-  data: User[];
-}
+type ResponseBody = { message: string };
 
-export default function handler(req: NextApiRequest, res: NextApiResponse<Data>) {
-  const simpleUsers: User[] = users.filter((user: User) => user.role === "user");
-
-  res.status(401).json({ data: simpleUsers });
+export default function handler(req: NextApiRequest, res: NextApiResponse<ResponseBody>) {
+  res.status(401).json({ message: 'Try again with the corrct password' });
 };
